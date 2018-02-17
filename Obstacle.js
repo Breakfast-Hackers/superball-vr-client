6940dd6969db68ced0beca0d0485f71a056c566a
Karl Bode <k.bode@reply.de>
2018-02-17 16:19:07 +0100

    Animated Obstacles

--- Obstacle.js
+++ Obstacle.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Sphere } from 'react-vr';
+import { Sphere, Animated, Easing } from 'react-vr';
 
 class Obstacle extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      positionY: new Animated.Value(1.5)
+    };
+  }
+
+  componentDidMount() {
+
+    let duration = this.props.speed * 5000;
+
+    Animated.timing(
+      this.state.positionY,
+      {
+        toValue: -1.5,
+        duration: duration,
+        isInteraction: false
+      }
+    ).start();
+  }
+
   render() {
-    return <Sphere
-      radius={0.05}
-      widthSegments={20}
-      heightSegments={12}
-      style={{
-        color: '#00F',
-        transform: [{ translate: [ 0, 1, -3] }]
-      }}
-    />
+    return (<Animated.View style={{ transform: [{ translateY: this.state.positionY }, { translate: [ this.props.positionX, 0, -3] }] }}>
+       <Sphere
+        radius={0.05}
+        widthSegments={20}
+        heightSegments={12}
+        style={{
+          color: this.props.color
+        }}
+      />
+    </Animated.View>);
   }
 }
 
--- index.vr.js
+++ index.vr.js
@@ -45,9 +45,9 @@ export default class HelloVRWorld extends React.Component {
     super(props);
 
     this.state = {
-      ballPosition: new Animated.Value(0)
+      ballPosition: new Animated.Value(0),
+      obstacles: []
     }
-
 
     this.goLeft = this.goLeft.bind(this);
     this.goRight = this.goRight.bind(this);
@@ -69,13 +69,19 @@ export default class HelloVRWorld extends React.Component {
             self.goRight();
             break;
          }
-
-
        });
 
       client.subscribe('/topic/commands', function (message) {
     	   const command = JSON.parse(message.body)
          console.log('command: ' + command.action);
+       });
+
+       client.subscribe('/topic/obstacles', message => {
+         let command = JSON.parse(message.body);
+         let obstacle = { positionX: command.position * 0.45, speed: 0.5, color: command.color };
+         var currentObstacles = this.state.obstacles;
+         currentObstacles.push(obstacle);
+         this.setState({ obstacles: currentObstacles });
        });
     });
 
@@ -101,17 +107,34 @@ export default class HelloVRWorld extends React.Component {
     ).start();
   }
 
+  // componentDidMount() {
+  //   setTimeout(() => this.tick(), 10);
+  // }
+  //
+  // tick() {
+  //
+  //
+  //
+  //   setTimeout(() => this.tick(), 10);
+  // }
+
   render() {
+
+    let obstacles = this.state.obstacles.map((model, index) => {
+      return <Obstacle key={"Obstacle-" + index} positionX={model.positionX} speed={model.speed} color={model.color} />
+    });
+
     return (
       <Animated.View>
         <Pano source={asset('chess-world.jpg')} />
         <Board />
         <Ball position={this.state.ballPosition} />
-        <Obstacle />
-        <DebugControls onLeft={this.goLeft} onRight={this.goRight} />
+        {obstacles}
       </Animated.View>
     );
   }
 };
+
+// <DebugControls onLeft={this.goLeft} onRight={this.goRight} />
 
 AppRegistry.registerComponent('HelloVRWorld', () => HelloVRWorld);
